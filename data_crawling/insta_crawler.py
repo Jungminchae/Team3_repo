b1e04c5514535a896718f1e6f9ae81ca3a01e4f6
Jungminchae <minchae3618@gmail.com>
2020-09-30 17:11:08 +0900

    fix some codes

--- data_crawling/insta_crawler.py
+++ data_crawling/insta_crawler.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import quote_plus
 from selenium.webdriver.common.keys import Keys
+import os
 
 class Insta_img_crawler:
     def __init__(self, tag, driver_path):
@@ -19,23 +20,30 @@ class Insta_img_crawler:
         body = self.driver.find_element_by_tag_name('body')
         while page_down_num > 0:
             body.send_keys(Keys.PAGE_DOWN)
-            time.sleep(1.5)
+            time.sleep(1.0)
             page_down_num -=1
     def start_crawling(self):
+        if os.path.isdir('./img') != True:
+            os.mkdir('./img')
+        if os.path.isdir('./img/{}'.format(self.tag)) != True:
+            os.mkdir('./img/{}'.format(self.tag))
+
         html = self.driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
         insta = soup.select('.v1Nh3.kIKUG._bz0w')
 
         n = 1
         for i in insta:
-            print('https://www.instagram.com'+ i.a['href'])
+            time.sleep(0.1)
+            # print('https://www.instagram.com'+ i.a['href'])
             imgUrl = i.select_one('.KL4Bh').img['src']
             with urlopen(imgUrl) as f:
-                with open('./img/' + self.tag + str(n) + '.jpg', 'wb') as h:
+                with open('./img/{}/'.format(self.tag) + self.tag + str(n) + '.jpg', 'wb') as h:
                     img = f.read()
                     h.write(img)
             n += 1
-            print(imgUrl)
-            print()
+            if n % 20 ==0:
+                print('{} 이미지 {}장 크롤링 성공'.format(self.tag, n))
+                print()
 
-        driver.close()
+        self.driver.close()
