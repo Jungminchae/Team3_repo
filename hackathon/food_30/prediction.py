13d0a83b5d068d774b460c522b12971e4bf17ad4
Jungminchae <minchae3618@gmail.com>
2020-11-18 11:52:11 +0900

    code fix

--- hackathon/food_30/prediction.py
+++ hackathon/food_30/prediction.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import tensorflow as tf 
 import numpy as np 
+from glob import glob
 from utils import FoodDataPaths
 
 class Prediction(FoodDataPaths):
@@ -8,15 +9,15 @@ class Prediction(FoodDataPaths):
     def _load_image(self, path, img_size) :
         img = tf.io.read_file(path)
         img = tf.image.decode_image(img, channels=3, expand_animations=False)
-        img = tf.image.resize(img, (IMG_SIZE, IMG_SIZE))
+        img = tf.image.resize(img, (img_size, img_size))
 
         label = int(path.split('/')[-2].split('_')[-1])
         return img, label
 
     def predict_test(self, model, img_size, nums):
-
+        test_image_path = glob(self.test_image_path)
         correct = 0
-        img_paths = np.random.choice(self.test_image_path, nums, replace=False)
+        img_paths = np.random.choice(test_image_path, nums, replace=False)
 
         for img_path in tqdm(img_paths):
             img, label = self._load_image(img_path, img_size)
@@ -27,3 +28,4 @@ class Prediction(FoodDataPaths):
             correct += 1
 
         print(f'정확도 : {round(correct / len(img_paths), 4) * 100}%')
+
